564a6e9d1c49f401106dc6c92b1704af4cdf1310
Nithin Christopher <christophernithin@gmail.com>
2023-03-19 12:31:10 +0530

    Added encoding feature

--- openfaasjavapoc-fn/src/main/java/com/openfaas/function/Handler.java
+++ openfaasjavapoc-fn/src/main/java/com/openfaas/function/Handler.java
@@ -1,6 +1,8 @@
 package com.openfaas.function;
 
-import com.openfaas.model.IHandler;
+import java.util.Base64;
+import java.util.Map;
+
 import com.openfaas.model.IResponse;
 import com.openfaas.model.IRequest;
 import com.openfaas.model.Response;
@@ -9,8 +11,10 @@ public class Handler extends com.openfaas.model.AbstractHandler {
 
     public IResponse Handle(IRequest req) {
         Response res = new Response();
-	    res.setBody("Hello, world!");
-
+        Map<String, String> query = req.getQuery();
+        String q = query.get("q");
+        String encodedString = Base64.getEncoder().encodeToString(q.getBytes());
+	    res.setBody("Original String = "+q+" Encoded value = "+encodedString);
 	    return res;
     }
 }
